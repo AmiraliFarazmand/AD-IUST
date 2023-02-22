68aef768264998df3f7d49d4f3df6b7f0d3baf78
amirfzd <amiralifarazmand80@gmail.com>
2023-02-22 17:27:30 +0330

    Solved LCS using memoizing.

--- HW1/fib.cpp
+++ HW1/fib.cpp
@@ -14,12 +14,7 @@ unsigned int fib(int n, unsigned int* mem){
 int main ()
 {
     int n; cin>>n;
-    // unsigned int memo [n+1] ;
-    // for (int i=0; i<n+1; i++){
-    //     memo[i]=-1;
-    // }
     unsigned long long int temp1=0,temp2=1, temp3;
-    // unsigned int res = fib(n, memo);
     for (int i=0; i<=n; i++){
         if (i>1) {
             temp3 = temp1 +temp2;
@@ -27,9 +22,6 @@ int main ()
             temp2 = temp3;
         }
     }
-
-    // int res = fib(n, memo)%1000000000;
-    // res = res% (1000000007);
     if (n<2){cout<<n;return 0;}
     unsigned long long int res = temp3 % (1000000007);
     cout<<res;
--- HW1/lcs.cpp
+++ HW1/lcs.cpp
@@ -1,30 +1,24 @@
+/*
+2 5 7 
+2 7
+>>>2
+*/
 #include <iostream>
-#include <bits/stdc++.h>
 #include <string>
 #include <vector>
-#include <iterator>
 #include <sstream>
+#include <iterator>
+#include <bits/stdc++.h>
 using namespace std;
-/*
-Used help from https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/ .
-*/
-int lcs( vector<int>array1, vector<int> array2, int l1, int l2 )
+int lcs(vector<int> array1, vector<int> array2, int l1, int l2, vector<vector<int>> &memo)
 {
-     int tabulate[l1 + 1][l2 + 1];
- 
-    for(int i = 0; i <= l1; i++)
-    {
-        for (int j = 0; j <= l2; j++)
-        {
-            if (i == 0 || j == 0)
-                tabulate[i][j] = 0;
-            else if (array1[i - 1] == array2[j - 1])
-                tabulate[i][j] = tabulate[i - 1][j - 1] + 1;
-            else
-                tabulate[i][j] = max(tabulate[i - 1][j], tabulate[i][j - 1]);
-        }
-    }
-    return tabulate[l1][l2];
+    if (l1 == 0 || l2 == 0)
+        return 0;
+    if (array1[l1 - 1] == array2[l2 - 1])
+        return memo[l1][l2] = 1 + lcs(array1, array2, l1 - 1, l2 - 1, memo);
+    else if (memo[l1][l2] == -1)
+        return memo[l1][l2] = max(lcs(array1, array2, l1, l2 - 1, memo), lcs(array1, array2, l1 - 1, l2, memo));
+    return memo[l1][l2];
 }
 
 int main()
@@ -32,16 +26,17 @@ int main()
     std::vector<int> arr;
     std::vector<int> arr2;
 
-        string temp; 
-        getline(cin, temp);
-        istringstream iss(temp);
-        copy(std::istream_iterator<int>(iss), std::istream_iterator<int>(), std::back_inserter(arr));
+    string temp;
+    getline(cin, temp);
+    istringstream iss(temp);
+    copy(std::istream_iterator<int>(iss), std::istream_iterator<int>(), std::back_inserter(arr));
 
-        getline(cin, temp);
-        istringstream iss2(temp);
-        copy(std::istream_iterator<int>(iss2), std::istream_iterator<int>(), std::back_inserter(arr2));
+    getline(cin, temp);
+    istringstream iss2(temp);
+    copy(std::istream_iterator<int>(iss2), std::istream_iterator<int>(), std::back_inserter(arr2));
 
     int len1 = arr.size(), len2 = arr2.size();
-    cout<<lcs(arr, arr2, len1, len2);
+    vector<vector<int>> memoize(len1 + 1, vector<int>(len2 + 1, -1));
+    cout << lcs(arr, arr2, len1, len2, memoize);
     return 0;
 }
