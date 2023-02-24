15416852dd27c990fb327dc2684e0c8a4663b7e6
amirfzd <amiralifarazmand80@gmail.com>
2023-02-25 00:43:04 +0330

    aliIsHungry is solved.

--- HW1/aliIsHungry.cpp
+++ HW1/aliIsHungry.cpp
@@ -7,37 +7,46 @@
 #include <ctime>
 using namespace std;
 
-double max(double s1, double s2)
+long int max(long int s1, long int s2)
 {
     if (s1 < s2)
         return s2;
     return s1;
 }
 
-double find_max_gain(int n, double p, double line1[], double line2[])
+long int find_max_gain(int n, long int p, long int line1[], long int line2[])
 {
-    double gains1[n] = {}, gains2[n] = {};
+    long int gains1[n] = {}, gains2[n] = {};
     gains1[0] = line1[0];
     gains2[0] = line2[0];
-    for (int i = 1; i < n; i++)
+    int i; 
+    // cout<<gains1[0]<<gains2[0]<<endl;
+    for (i = 1; i <n; i++)
     {
         gains1[i] = max(gains1[i - 1] + line1[i], gains2[i - 1] + line1[i] - p);
         gains2[i] = max(gains2[i - 1] + line2[i], gains1[i - 1] + line2[i] - p);
     }
+    // cout << endl;
+    // for (int i =0; i < n; i++)
+    //     cout << gains1[i] << ' ';
+    // cout << endl;
+    // for (int i =0; i < n; i++)
+    //     cout << gains2[i] << ' ';
+    // cout << endl;
     return max(gains1[n - 1], gains2[n - 1]);
 }
 
 int main()
 {
-    int n;
-    double p;
+    unsigned int n;
+    long int p;
     cin >> n >> p;
-    double line1[n], line2[n];
+    long int line1[n], line2[n];
     for (int i = 0; i < n; i++)
         cin >> line1[i];
     for (int i = 0; i < n; i++)
         cin >> line2[i];
-    int res = find_max_gain(n, p, line1, line2);
+    long int res = find_max_gain(n, p, line1, line2);
     cout << res;
     return 0;
 }
