4a2f76428d160d545dec38eb3f4efdefe4f5eb5e
amirfzd <amiralifarazmand80@gmail.com>
2023-02-24 15:25:22 +0330

    working on numberMixer.

---
+++ HW1/numberMaker.cpp
@@ -0,0 +1,70 @@
+// Coin change Permutations problem:
+// Useful links:
+//  https://cppsecrets.com/users/101611099712197110107114971065149535048484864103109971051084699111109/C00-The-Coin-Change-Permutation-Problem-.php
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <bits/stdc++.h>
+#include <ctime>
+
+using namespace std;
+// int memo[100000];
+vector<int> memo(100000, 0);
+int coin_change(int target, int n, int nums[])
+{
+    // cout<<"called func for( "<<target<<" )\n";
+    // if (memo[target] != 0)
+    // {
+    //     return memo[target];
+    // }
+    // if (target == 0)
+    //     return 1;
+    // int flag = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (nums[i] < target)
+    //     {
+    //         flag =1;
+    //         int x = coin_change(target-nums[i], n, nums);
+    //         // if (x != -1)
+    //             memo[target] += x;
+    //     }
+    // }
+    // // if (flag == 1)
+    // //     return memo[target];
+    // if (flag == 0)
+    //     return 0;
+
+    memo[0] = 1;
+    for (int i = 1; i < target; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j] <= i)
+            {
+                memo[i] += memo[i - nums[j]];
+            }
+        }
+    }
+    return memo[target];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int numbs[n];
+    for (int i = 0; i < n; i++)
+        cin >> numbs[i];
+    int target;
+    cin >> target;
+    // for (int i=0; i<100000; i++)
+    //     memo[i]= -1
+    int res = coin_change(target, n, numbs);
+    cout <<res<<' '<<memo[target]<<endl;
+    for (int i=0; i<10; i++)
+        cout <<i<<":"<<memo[i]<<"  ";
+    cout << memo[target];
+    return 0;
+}
--- HW1/stringMixer.cpp
+++ HW1/stringMixer.cpp
@@ -13,19 +13,17 @@ Matrix memo(1001, vector<int>(1001, 3));
 
 int string_mixer(string s1, string s2, string s3, int p1, int p2, int p3, int n1, int n2)
 {
-    cout << "string_mixer called("
-            " "
-         << p1 << " " << p2 << " " << p3 << ")\n";
+    // cout << "string_mixer called( " << p1 << " " << p2 << " " << p3 << ")\n";
     if (memo[p1][p2] != 3)
         return memo[p1][p2];
     if (p1 == n1 && p2 == n2)
         return 1;
 
-    if (s1[p1] == s3[p3] && s2[p2] == s3[p3])
+    if (s1[p1] == s3[p3] && s2[p2] == s3[p3] && p1 < n1 && p2 < n2)
     {
         int go_row = string_mixer(s1, s2, s3, p1 + 1, p2, p3 + 1, n1, n2);
         int go_col = string_mixer(s1, s2, s3, p1, p2 + 1, p3 + 1, n1, n2);
-        return memo[p1][p2] = go_row || go_col;
+        return memo[p1][p2] = go_row | go_col;
     }
     if (p1 < n1 && s1[p1] == s3[p3])
     {
@@ -43,8 +41,7 @@ int main()
     string s1, s2, s3;
     cin >> s1 >> s2 >> s3;
     int p1 = 0, p2 = 0, p3 = 0;
-    bool flag = 1;
-    if (s3.length() != s1.length() + s2.length())
+    if (s3.length() != (s1.length() + s2.length()))
     {
         cout << "False";
         return 1;
@@ -58,7 +55,7 @@ int main()
         cout << endl;
     }
     cout << "*****\n";
-    // int memo [s1.length()+1][s2.length()+1];
+    // // int memo [s1.length()+1][s2.length()+1];
     cout << result;
     return 0;
 }
