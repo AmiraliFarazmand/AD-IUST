b4e16ae36d3e2524e94d026e4e6e6a7fb38cf077
amirfzd <amiralifarazmand80@gmail.com>
2023-03-11 23:59:48 +0330

    LCS2 is solved.

--- HW2/lcs2.cpp
+++ HW2/lcs2.cpp
@@ -1,3 +1,4 @@
+// LCS problem for 3 arrays:
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,15 +6,34 @@
 #include <iterator>
 #include <bits/stdc++.h>
 using namespace std;
-int lcs(vector<int> array1, vector<int> array2, int l1, int l2, int l3, vector<vector<int>> &memo)
+
+int find_max_in_3(int a, int b, int c)
 {
-    if (l1 == 0 || l2 == 0 || l3==0)
+    if (a >= b)
+    {
+        if (a >= c)
+            return a;
+        else
+            return c;
+    }
+    if (b >= c)
+        return b;
+    return c;
+}
+int lcs(vector<int> array1, vector<int> array2, vector<int> array3, int l1, int l2, int l3, vector<vector<vector<int>>> &memo)
+{
+    if (l1 == 0 || l2 == 0 || l3 == 0)
         return 0;
-    if (array1[l1 - 1] == array2[l2 - 1])
-        return memo[l1][l2] = 1 + lcs(array1, array2, l1 - 1, l2 - 1, memo);
-    else if (memo[l1][l2] == -1)
-        return memo[l1][l2] = max(lcs(array1, array2, l1, l2 - 1, memo), lcs(array1, array2, l1 - 1, l2, memo));
-    return memo[l1][l2];
+    
+    if (array1[l1 - 1] == array2[l2 - 1] && array1[l1 - 1] == array3[l3 - 1])
+        return memo[l1][l2][l3] = 1 + lcs(array1, array2, array3, l1 - 1, l2 - 1, l3 - 1, memo);
+
+    else if (memo[l1][l2][l3] == -1)
+        return memo[l1][l2][l3] = find_max_in_3(lcs(array1, array2, array3, l1 - 1, l2, l3, memo),
+                                                lcs(array1, array2, array3, l1, l2 - 1, l3, memo),
+                                                lcs(array1, array2, array3, l1, l2, l3 - 1, memo));
+
+    return memo[l1][l2][l3]; // whhen memo is not -1
 }
 
 int main()
@@ -29,5 +49,20 @@ int main()
         cin >> arr2[i];
     for (int i = 0; i < k; i++)
         cin >> arr3[i];
-    
+    vector<vector<vector<int>>> memoize(n + 1, vector<vector<int>>(m + 1, vector<int>(k + 1, -1)));
+    cout << lcs(arr1, arr2, arr3, n, m, k, memoize);
 }
+/*
+5 5 7
+1 2 3 4 5
+5 4 3 2 1
+5 4 3 2 1 6 7
+>>>1
+
+
+5 5 7
+1 2 3 4 5
+5 4 3 4 1
+5 4 3 2 4 6 7
+>>>2
+*/
