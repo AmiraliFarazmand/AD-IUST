14eac090e5054c04576341365f4dd077e61ca756
amirfzd <amiralifarazmand80@gmail.com>
2023-03-12 10:50:14 +0330

    LIS2 has completed.

--- HW2/lis2.cpp
+++ HW2/lis2.cpp
@@ -1,4 +1,6 @@
 // Longest Increasing Subarray in order of n.log n
+// Implemented using help from leetcode:(https://leetcode.com/problems/longest-increasing-subsequence/solutions/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation/)
+
 #include <string>
 #include <vector>
 #include <sstream>
@@ -6,60 +8,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int CeilIndex(vector<int>& v, int l, int r, int key)
+int lis(vector<int> array)
 {
-    while (r - l > 1) {
-        int m = l + (r - l) / 2;
-        if (v[m] >= key)
-            r = m;
-        else
-            l = m;
+    int sz = array.size();
+    int len = 0;
+    vector<int> tails(sz, 0);
+    for (int indx = 0; indx < sz; indx++)
+    {
+        int i = 0, j = len;
+        while (i < j)
+        {
+            int m = (i + j) / 2;
+            if (tails[m] < array[indx])
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = array[indx];
+        len = max(i+1 , len);
     }
-  
-    return r;
-}
-
-
-
-int lis(vector<int>& v)
-{
-    if (v.size() == 0)
-        return 0;
-  
-    vector<int> tail(v.size(), 0);
-    int length = 1; // always points empty slot in tail
-  
-    tail[0] = v[0];
-    for (size_t i = 1; i < v.size(); i++) {
-  
-        // new smallest value
-        if (v[i] < tail[0])
-            tail[0] = v[i];
-  
-        // v[i] extends largest subsequence
-        else if (v[i] > tail[length - 1])
-            tail[length++] = v[i];
-  
-        // v[i] will become end candidate of an existing
-        // subsequence or Throw away larger elements in all
-        // LIS, to make room for upcoming greater elements
-        // than v[i] (and also, v[i] would have already
-        // appeared in one of LIS, identify the location
-        // and replace it)
-        else
-            tail[CeilIndex(tail, -1, length - 1, v[i])] = v[i];
-    }
-  
-    return length;
+    return len;
 }
 int main()
 {
     int n;
     cin >> n;
-    // int arr[n]
-    vector <int> arr(n);
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     cout << lis(arr);
     return 0;
 }
+/*
+5
+2 7 4 3 8
+
+*/
